63370a13bb0036531d60214f0ef197bc391ea8bc
onkyo <championjared@gmail.com>
2016-04-10 13:18:33 +1000

    Init Lab2 File
    Refined input to self-function

--- cosc2140/lab2/lab2_binIn.c
+++ cosc2140/lab2/lab2_binIn.c
@@ -1,21 +1,27 @@
 #include <iostream>
 
+int rBinMap(std::string filename)
+{
+  std::cout << "Input File: " << filename << "\n";
+
+  return 0;
+}
+
+
+//Main Init Function
 int main(int argc, char **argv)
 {
 
   //read in cmd arguments
   for(int i = 1; i < argc; ++i)
   {
-    //cast as string so can use string sub-functions
     std::string s = argv[i];
-
-    //if difference is 0, str are identical
-    //_-- On flag match, init task
     if(s.compare("-f") == 0)
     {
-      std::cout << "Input File: " << argv[i + 1] << "\n";
+      rBinMap(argv[i+1]);
     }
   }
+
 
   return 0;
 }
